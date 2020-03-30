b5513996e175b37a4bc0c628e20a64740f7a6b9e
Nico <nicolas.judalet@gmail.com>
2020-03-30 12:21:34 +0200

    Authenticate Slack calls

--- hello/tests/test_views.py
+++ hello/tests/test_views.py
@@ -32,8 +32,13 @@ class TestSlack(TestCase):
             "actions": [{"value": "form_not_filled",}],
         },
     )
+    @patch("hello.views.authenticate_call", return_value=True)
     def test_slack(
-        self, deserialize_reminder_payload, update_status, send_reminder_acknowledgement
+        self,
+        authenticate_call,
+        deserialize_reminder_payload,
+        update_status,
+        send_reminder_acknowledgement
     ):
         """Tests the slack endpoint"""
         request = self.factory.post(
@@ -42,9 +47,41 @@ class TestSlack(TestCase):
 
         response = slack(request)
 
+        authenticate_call.assert_called_with(request)
         deserialize_reminder_payload.assert_called_with(request)
         update_status.assert_called_with("SLACK_ID", False)
         send_reminder_acknowledgement.assert_called_with(
             "https://slack.com/response_url", False
         )
         self.assertEqual(response.status_code, 200)
+
+    @patch("hello.views.send_reminder_acknowledgement")
+    @patch("hello.views.update_status")
+    @patch(
+        "hello.views.deserialize_reminder_payload",
+        return_value={
+            "user": {"id": "SLACK_ID"},
+            "response_url": "https://slack.com/response_url",
+            "actions": [{"value": "form_not_filled",}],
+        },
+    )
+    @patch("hello.views.authenticate_call", return_value=False)
+    def test_unauthenticated_call(
+        self,
+        authenticate_call,
+        deserialize_reminder_payload,
+        update_status,
+        send_reminder_acknowledgement
+    ):
+        """Tests unauthenticated calls get answered 403 and are not processed"""
+        request = self.factory.post(
+            "/api/slack", data=self.request_payload, content_type="application/json"
+        )
+
+        response = slack(request)
+
+        authenticate_call.assert_called_with(request)
+        assert not deserialize_reminder_payload.called, 'deserialize_reminder_payload should not have been called'
+        assert not update_status.called, 'update_status should not have been called'
+        assert not send_reminder_acknowledgement.called, 'send_reminder_acknowledgement should not have been called'
+        self.assertEqual(response.status_code, 403)
